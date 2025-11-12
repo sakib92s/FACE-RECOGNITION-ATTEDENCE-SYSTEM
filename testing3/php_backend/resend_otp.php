2494d9a342821c8c41b57c422a5ac26a830b356c
Sakib Shaikh <147747620+sakib92s@users.noreply.github.com>
2025-11-12 17:22:03 +0530

    Add files via upload

---
+++ testing3/php_backend/resend_otp.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+include("connect.php");
+
+if (isset($_SESSION['mobile']) && isset($_SESSION['pending_employee'])) {
+  
+    $otp = rand(100000, 999999);
+    $_SESSION['OTP'] = $otp;
+    $mobile = $_SESSION['mobile'];
+
+    $API = "YOUR-API-KEY"; 
+    $URL = "https://sms.renflair.in/V1.php?API=$API&PHONE=$mobile&OTP=$otp";
+
+    $curl = curl_init($URL);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($curl);
+    curl_close($curl);
+
+    echo json_encode(['success' => true]);
+} else {
+    echo json_encode(['success' => false]);
+}
+?>
